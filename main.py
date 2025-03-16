ba9b1b0df44c1c6e4862d7b174fb253e937121b5
jayteaare <tallisjordan4@gmail.com>
2025-03-16 15:05:59 -0400

    moved functions to stats

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import sys
-from stats import get_num_words
-
-def char_count(file_contents):
-    character_count = {}
-    # make all the text lowercase
-    lower_contents = file_contents.lower()
-    for char in lower_contents:
-        #only alphanumeric
-        if char.isalpha():
-            # check if key exists, if not create it w/ value of 0, +1 if existing
-            character_count[char] = character_count.get(char, 0) + 1
-    return character_count
+from stats import word_count, char_count
 
 def main(filepath):
     # open and read contents
@@ -19,7 +8,7 @@ def main(filepath):
 
     # get the word count
     words = word_count(file_contents)
-    print(f"Word count: {words}\n")
+    print(f"{words} words found in the document")
 
     # get the character count
     characters = char_count(file_contents)
@@ -27,12 +16,12 @@ def main(filepath):
     sorted_chars = sorted(characters.items(), key=lambda item: item[1], reverse=True)
     # print each result
     for char, count in sorted_chars:
-        print(f"The '{char}' character was found {count} times")
+        print(f"{char}: {count}")
 
 if __name__ == "__main__":
     #check for arg
     if len(sys.argv) != 2:
-        print ("Usage: python3 main.py <filepath>")
+        print ("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     filepath = sys.argv[1]
     main(filepath)
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
+def char_count(file_contents):
+    character_count = {}
+    # make all the text lowercase
+    lower_contents = file_contents.lower()
+    for char in lower_contents:
+        #only alphanumeric
+        if char.isalpha():
+            # check if key exists, if not create it w/ value of 0, +1 if existing
+            character_count[char] = character_count.get(char, 0) + 1
+    return character_count
+
 def word_count(file_contents):
     # count words from contents
     return len(file_contents.split())
